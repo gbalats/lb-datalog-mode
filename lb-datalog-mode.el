93f4749f492f2a677fee4607b5537b17c3f15178
George Balatsouras <gbalats@gmail.com>
2014-10-16 19:05:57 +0300

    Adding library headers.

--- lb-datalog-mode.el
+++ lb-datalog-mode.el
@@ -1,13 +1,40 @@
 ;;; lb-datalog-mode.el --- major mode for editing Datalog code
 
-;; Author: George Balatsouras
-;; Keywords: LogicBlox Datalog
+;; Copyright (C) 2014, George Balatsouras
+;;
+;; Author: George Balatsouras <gbalats(at)gmail(dot)com>
+;; Maintainer: George Balatsouras <gbalats(at)gmail(dot)com>
+;; Created: 26 Aug 2014
+;; Keywords: convenience, languages
+
+;;
+;; This file is NOT part of Emacs.
+;;
+;; This program is free software; you can redistribute it and/or
+;; modify it under the terms of the GNU General Public License as
+;; published by the Free Software Foundation; either version 2 of
+;; the License, or (at your option) any later version.
+;;
+;; This program is distributed in the hope that it will be
+;; useful, but WITHOUT ANY WARRANTY; without even the implied
+;; warranty of MERCHANTABILITY or FITNESS FOR A PARTICULAR
+;; PURPOSE.  See the GNU General Public License for more details.
+;;
+;; You should have received a copy of the GNU General Public
+;; License along with this program; if not, write to the Free
+;; Software Foundation, Inc., 59 Temple Place, Suite 330, Boston,
+;; MA 02111-1307 USA
 
 
 ;;; Commentary:
 
-;; Put this file into your load-path and the following into your ~/.emacs:
-;;   (require 'lb-datalog-mode)
+;; This is a major mode for the LogicBlox Datalog language.  It
+;; provides syntax highlighting, movement functions, and expansions.
+;;
+;; Put this file into your load-path and the following into your
+;; `~/.emacs':
+;;
+;; (require 'lb-datalog-mode)
 
 
 ;;; Code:
